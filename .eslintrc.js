74f203407bb7f5f7343af1d0866f38ae22f92fac
LonM <l@CATRA>
2019-10-19 11:37:23 +0100

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true,
+        "webextensions": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 2018
+    },
+    "rules": {
+        "indent": [
+            "error",
+            4
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    },
+    "globals": {
+        "vivaldi": "readonly"
+    }
+};
---
+++ README.md
@@ -0,0 +1,10 @@
+# Vivaldi Modders API
+References to assist in modding vivaldi.
+
+## apiGen.js
+Run this in the vivaldi console and it will output an HTML file of the current internal API.
+
+**WARNING!** This executes methods and could end up being destructive, so run in a clean profile / user directory.
+
+## api.html
+A description of the `vivaldi.*` api namespace.
