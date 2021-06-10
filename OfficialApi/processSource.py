7a7977b318f5bdb179a619c3abbe93487f2df5b4
lonm <LonMcGregor@users.noreply.github.com>
2021-06-10 18:58:30 +0100

    4.0

--- OfficialApi/processSource.py
+++ OfficialApi/processSource.py
@@ -2,7 +2,7 @@ import json, glob, os, re
 from json import JSONDecodeError
 import jstyleson
 
-SOURCE_PATH = r'C:\Users\L\Downloads\vivaldi-source_3.8.2259\vivaldi-source'
+SOURCE_PATH = r'C:\Users\L\Downloads\vivaldi-source_4.0.2312.tar\vivaldi-source_4.0.2312\vivaldi-source'
 
 def getAllApiDefsInSourceBundle():
     return glob.glob(os.path.join(SOURCE_PATH, 'extensions/schema/*.json'))
